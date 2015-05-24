3dd32c8325c8a5e57fc6e32c2cd399fe6cfb7718
mdczaplicki <mdczaplicki@gmail.com>
2015-05-25 00:14:21 +0200

    recent

--- Main.py
+++ Main.py
@@ -42,23 +42,62 @@ enum = range(len(minT))
 c1 = (rand.random_integers(0, 14000), rand.random_integers(0, 100))
 c2 = (rand.random_integers(0, 14000), rand.random_integers(0, 100))
 c3 = (rand.random_integers(0, 14000), rand.random_integers(0, 100))
+old_c1 = (-1000, -1000)
+old_c2 = (-1000, -1000)
+old_c3 = (-1000, -1000)
 
 c1p = []
-c2p = []
-c3p = []
+c1x = []
+c1y = []
 
-for point in enum:
-    dist = []
-    x1 = point - c1[0]
-    y1 = minT[point] - c1[1]
-    dist1 = sqrt(x1**2 + y1**2)
-    x2 = point - c2[0]
-    y2 = minT[point] - c2[1]
-    dist2 = sqrt(x2**2 + y2**2)
-    x3 = point - c3[0]
-    y3 = minT[point] - c3[1]
-    dist3 = sqrt(x3**2 + y3**2)
-    c1p.append((point, minT[point])) if dist1 < dist2 and dist1 < dist3 else c2p.append((point, minT[point])) if dist2 < dist3 else c3p.append((point, minT[point]))
+c2p = []
+c2x = []
+c2y = []
+
+c3p = []
+c3x = []
+c3y = []
+
+
+def new_cluster():
+    global c1
+    global c2
+    global c3
+
+    c1p.clear()
+    c1x.clear()
+    c1y.clear()
+    c2p.clear()
+    c2x.clear()
+    c2y.clear()
+    c3p.clear()
+    c3x.clear()
+    c3y.clear()
+    for point in enum:
+        x1 = point - c1[0]
+        y1 = minT[point] - c1[1]
+        dist1 = sqrt(x1**2 + y1**2)
+        x2 = point - c2[0]
+        y2 = minT[point] - c2[1]
+        dist2 = sqrt(x2**2 + y2**2)
+        x3 = point - c3[0]
+        y3 = minT[point] - c3[1]
+        dist3 = sqrt(x3**2 + y3**2)
+        c1p.append((point, minT[point])) if dist1 < dist2 and dist1 < dist3 else c2p.append((point, minT[point])) if dist2 < dist3 else c3p.append((point, minT[point]))
+        c1x.append(point) if dist1 < dist2 and dist1 < dist3 else c2x.append(point) if dist2 < dist3 else c3x.append(point)
+        c1y.append(minT[point]) if dist1 < dist2 and dist1 < dist3 else c2y.append(minT[point]) if dist2 < dist3 else c3y.append(minT[point])
+
+    c1 = (functools.reduce(lambda x, y: x + y, c1x) / len(c1x), functools.reduce(lambda x, y: x + y, c1y) / len(c1y))
+    c2 = (functools.reduce(lambda x, y: x + y, c2x) / len(c2x), functools.reduce(lambda x, y: x + y, c2y) / len(c2y))
+    c3 = (functools.reduce(lambda x, y: x + y, c3x) / len(c3x), functools.reduce(lambda x, y: x + y, c3y) / len(c3y))
+
+quantisation_error = 10
+
+while not (abs(old_c1[0] - c1[0]) < quantisation_error * 10 and abs(old_c1[1] - c1[1]) < quantisation_error and abs(old_c2[0] - c2[0]) < quantisation_error * 10 and abs(old_c2[1] - c2[1]) < quantisation_error and abs(old_c3[0] - c3[0]) < quantisation_error * 10 and abs(old_c3[1] - c3[1]) < quantisation_error):
+    old_c1 = c1
+    old_c2 = c2
+    old_c3 = c3
+    new_cluster()
 
 scatter(*zip(*c1p), marker='x', c='red')
 scatter(*zip(*c2p), marker='x', c='blue')
