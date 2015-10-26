4602cf9655b68162bf022c27181075cac8d9211d
mdczaplicki <mdczaplicki@gmail.com>
2015-10-26 13:02:20 +0100

    Start of neural network

--- test.py
+++ test.py
@@ -3,31 +3,39 @@ import configparser
 from sklearn import datasets, svm
 
 
-def neuro() -> None:
+def readFromIni() -> list:
     config = configparser.RawConfigParser()
     config.read('file.ini')
     iClusters = config.getint('cluster_points', 'clusters_number')
 
+    vPoints = [[] for _ in range(iClusters)]
     for i in range(iClusters):
-        vPoints = config.get('cluster_points', str(i))
-        assert isinstance(vPoints, str)
-        vPoints = vPoints.replace('(', '').replace(')', '').split('#')
-        vPoints = [x.split(',') for x in vPoints]
-        vPoints = [(float(x[0]), float(x[1])) for x in vPoints][:2]
+        temp = config.get('cluster_points', str(i))
+        assert isinstance(temp, str)
+        temp = temp.replace('(', '').replace(')', '').split('#')
+        temp = [x.split(',') for x in temp]
+        vPoints[i] = [(float(x[0]), float(x[1])) for x in temp]
+
+    return vPoints
 
 
-def new() -> None:
-    iris = datasets.load_iris()
-    X = [
-        [0, 0],
-        [10, 10],
-        [100, 100],
-    ]
-    y = [0, 1, 3]
+def learnNeuralNetwork(vLearning) -> svm.SVC:
     clf = svm.SVC()
-    clf.fit(X, y)
+    X = []
+    y = []
+    for iCluster in range(len(vLearning)):
+        for point in vLearning[iCluster]:
+            X.append(point)
+            y.append(iCluster)
 
-    print(clf.predict([500, 4.9999999999999999]))
+    clf.fit(X, y)
+    print(clf.predict((37086, 24925)))
 
 if __name__ == "__main__":
-    new()
+    vPoints = readFromIni()
+    vLearning, vAsking = [], []
+    for vClusterPoints in vPoints:
+        vLearning.append(vClusterPoints[:-5])
+        vAsking.append(vClusterPoints[-5:])
+
+    learnNeuralNetwork(vLearning)
